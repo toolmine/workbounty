0fd93b9d7150c68d877a7ddf61d028bcc4f24eee
pacharyazc <pacharya@zerochaos.com>
2016-05-12 18:40:33 +0530

    Fix Layout Changes

--- DemoWorkBounty/assets/js/jquery-search.js
+++ DemoWorkBounty/assets/js/jquery-search.js
@@ -1,4 +1,35 @@
-﻿
+﻿function handleEnter(inField, e) {
+    var charCode;
+   
+    if (e && e.which) {
+        charCode = e.which;
+    } else if (window.event) {
+        e = window.event;
+        charCode = e.keyCode;
+    }
+
+    if (charCode == 13) {
+        var id = $("#SearchItem").val();
+        if (id == null) {
+            alert("Please enter Workitem Name");
+        }
+        else {
+            //var url = "/home/SearchWorkitem?searchWorkitemValue=" + encodeURIComponent(id);
+            //document.location.href = url;
+
+           $.ajax({
+                url: "/Home/SearchWorkitem",
+                type: 'POST',
+                data: {'searchWorkitemValue':id},
+                success: function (data)
+                {
+                    var url = "/SearchWorkitem";
+                    document.location.href = url;
+                }
+            });
+        }
+    }
+}
 
 function SearchResult() {
    
@@ -7,12 +38,11 @@ function SearchResult() {
         var id = $("#SearchItem").val();
         if (id == null) {
             alert("Please enter Workitem Name");
-          }
+        }
         else {
             var url = "/home/SearchWorkitem?searchWorkitemValue=" + encodeURIComponent(id);
             document.location.href = url;
         }
-   
 
    
 };
