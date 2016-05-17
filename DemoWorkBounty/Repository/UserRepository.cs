10a7d3fff9fab2a9dc37c15e5edac6fa835e5361
pacharyazc <pacharya@zerochaos.com>
2016-05-17 11:01:54 +0530

    Forgot Password Email Query

--- DemoWorkBounty/Repository/UserRepository.cs
+++ DemoWorkBounty/Repository/UserRepository.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Web.Http;
 using DemoWorkBounty.Models;
 using DemoWorkBounty;
+using System.Net.Mail;
 
 namespace DemoWorkBounty.Repository
 {
@@ -74,6 +75,44 @@ namespace DemoWorkBounty.Repository
 
         }
 
+
+        public UserInfo ForgotPasswordValidation(UserInfo userLoginData)
+        {
+            try
+            {
+                var checkLoginData = entity.UserInfoes.Where(a => a.Email.Equals(userLoginData.Email)).FirstOrDefault();
+
+                if (checkLoginData == null)
+                {
+                    return checkLoginData;
+                }
+                else
+                {
+                    var getPasswordDetail = entity.UserInfoes.Where(a => a.Email.Equals(userLoginData.Email)).Select(a => a.Password).FirstOrDefault();
+                    MailMessage mm = new MailMessage("help.workbounty@gmail.com", userLoginData.Email);
+                    mm.Subject = "Workbounty Password Recovery";
+                    mm.Body = string.Format("Hi {0},<br /><br />Your password is {1}.<br /><br />Thank You.", checkLoginData.Email, getPasswordDetail);
+                    mm.IsBodyHtml = true;
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    NetworkCredential NetworkCred = new NetworkCredential();
+                    NetworkCred.UserName = "help.workbounty@gmail.com";
+                    NetworkCred.Password = "workbountyproject1234";
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = 587;
+                    smtp.Send(mm);
+
+                    return checkLoginData;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
 
 
 
---
+++ DemoWorkBounty/assets/js/forgotPassword.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    $("#alertMessage").hide();
+    $("#passwordLinkSuccess").hide();
+    $("#alertBlankInputMessage").hide();
+
+});
+$(function () {
+    $("#forgotButton").click(function (e) {
+
+        e.preventDefault();
+        var id = {
+            "Email": $("#emailID").val(),
+        };
+
+        if (id.Email == "") {
+            $("#alertBlankInputMessage").show();
+        }
+        else {
+            $.ajax({
+                url: "/Home/ForgotPassword/",
+                type: "POST",
+                contentType: "application/json;charset=utf-8",
+                data: JSON.stringify(id),
+                dataType: "json",
+                success: function (response) {
+                    console.log(response);
+                    if (response.success) {
+                        $("#passwordLinkSuccess").hide();
+                        location.href = response.redirectURL;
+                    }
+                    else {
+                        $("#alertMessage").show();
+                    }
+                },
+
+                error: function (x, e) {
+                    $("#alertMessage").show();
+                }
+            });
+        }
+    });
+
+});
