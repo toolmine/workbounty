6b35c6a65e5cae29adc43648bc11c4f40c739887
jmacwanzc <jmacwan@zerochaos.com>
2016-06-02 04:59:11 -0400

    file upload size changes

--- DemoWorkBounty/assets/js/jquery-addworkitem.js
+++ DemoWorkBounty/assets/js/jquery-addworkitem.js
@@ -23,141 +23,145 @@ $(document).ready(function () {
 
 });
 
-
-
 function AddWorkitem() {
 
     var uploadedFileSize = 0;
     var uploadedFile = document.getElementById('myFile');
     if (uploadedFile.value != "") {
-    {
-        uploadedFileSize = uploadedFile.files[0].size;
-    }
-
-    $("#form").submit();
-
-    var d = new Date();
-    var dueDate = $("#DueDate").val();
-    var startDate = $("#StartDate").val();
-    var startDateObject = new Date(startDate);
-    var dueDateObject = new Date(dueDate);
-    var fileInput = document.getElementById('myFile');
-    var fileName = fileInput.value.split(/(\\|\/)/g).pop();
-
-    var newitem = {};
-    newitem.Title = $("#Title").val();
-    newitem.Summary = $("#Summary").val();
-    newitem.StartDate = startDateObject;
-    newitem.DueDate = dueDateObject;
-    newitem.PublishedTo = $("#TeamList").val();
-    newitem.DocumentFilePath = fileName;
-    newitem.ProposedReward = sessionStorage.getItem('key1');
-    newitem.Amount = sessionStorage.getItem('key2');
-    newitem.CreatedBy = $("#Userid").val();
-    newitem.CreatedDateTime = d;
-    newitem.ModifyBy = $("#Userid").val();
-    newitem.ModifyDateTime = d;
-    newitem.Status = true;
-    newitem.Remarks = "Good";
-    newitem.IsOpenForGroup = true;
-    newitem.Content = uploadedFileSize;
-
-    if ($("#Title").val() == "") {
-        $("#TitleError").text("Title is Required");
-    }
-    else if ($("#Summary").val() == "") {
-        $("#SummaryError").text("Summary is Required");
-    }
-
-    else if ($("#StartDate").val() == "") {
-        $("#StartdateError").text("Start Date is Required");
-    }
-
-    else if ($("#DueDate").val() == "") {
-        $("#DuedateError").text("Due Date is Required");
-
-    }
-
-    else {
-        var summaryVal = $("#Summary").val();
-        var summaryText = summaryVal.indexOf(' ') >= 2;
-
-        if (summaryText == true)
         {
-            $("#SummaryError").text("Maximum character limit exists");
+            uploadedFileSize = uploadedFile.files[0].size;
+            if (uploadedFileSize > 4194304) {
+                $("#filesizebig").text("File is too big!");
+                return null;
+            };
         }
+    }
+
+        $("#form").submit();
+
+        var d = new Date();
+        var dueDate = $("#DueDate").val();
+        var startDate = $("#StartDate").val();
+        var startDateObject = new Date(startDate);
+        var dueDateObject = new Date(dueDate);
+        var fileInput = document.getElementById('myFile');
+        var fileName = fileInput.value.split(/(\\|\/)/g).pop();
+
+        var newitem = {};
+        newitem.Title = $("#Title").val();
+        newitem.Summary = $("#Summary").val();
+        newitem.StartDate = startDateObject;
+        newitem.DueDate = dueDateObject;
+        newitem.PublishedTo = $("#TeamList").val();
+        newitem.DocumentFilePath = fileName;
+        newitem.ProposedReward = sessionStorage.getItem('key1');
+        newitem.Amount = sessionStorage.getItem('key2');
+        newitem.CreatedBy = $("#Userid").val();
+        newitem.CreatedDateTime = d;
+        newitem.ModifyBy = $("#Userid").val();
+        newitem.ModifyDateTime = d;
+        newitem.Status = true;
+        newitem.Remarks = "Good";
+        newitem.IsOpenForGroup = true;
+        newitem.Content = uploadedFileSize;
+
+        if ($("#Title").val() == "") {
+            $("#TitleError").text("Title is Required");
+        }
+        else if ($("#Summary").val() == "") {
+            $("#SummaryError").text("Summary is Required");
+        }
+
+        else if ($("#StartDate").val() == "") {
+            $("#StartdateError").text("Start Date is Required");
+        }
+
+        else if ($("#DueDate").val() == "") {
+            $("#DuedateError").text("Due Date is Required");
+
+        }
+
         else {
-            $.ajax({
-                type: "POST",
-                url: '/Home/AddWorkitem/',
-                data: JSON.stringify({ addWorkitemData: newitem }),
-                contentType: "application/json;charset=utf-8",
-                processData: true,
-                success: function (response) {
-                    console.log(response);
-                    if (response.IsSuccess) {
-                        location.href = response.redirectURL;
-                    }
-                    else {
+            var summaryVal = $("#Summary").val();
+            var summaryText = summaryVal.indexOf(' ') >= 2;
+
+            if (summaryText == true) {
+                $("#SummaryError").text("Maximum character limit exists");
+            }
+            else {
+                $.ajax({
+                    type: "POST",
+                    url: '/Home/AddWorkitem/',
+                    data: JSON.stringify({ addWorkitemData: newitem }),
+                    contentType: "application/json;charset=utf-8",
+                    processData: true,
+                    success: function (response) {
+                        console.log(response);
+                        if (response.IsSuccess) {
+                            location.href = response.redirectURL;
+                        }
+                        else {
+                            $("#alertMessage").show();
+                        }
+                    },
+                    error: function (xhr) {
                         $("#alertMessage").show();
                     }
-                },
-                error: function (xhr) {
-                    $("#alertMessage").show();
-                }
-            });
+                });
+            }
         }
+
+
+        function isTextKey(evt) {
+            var charCode = (evt.which) ? evt.which : event.keyCode;
+            if (charCode > 31 && charCode < 65
+              && (charCode < 97 || charCode > 122))
+                return false;
+
+            return true;
+        }
+
+        function isNumberKey(evt) {
+            var charCode = (evt.which) ? evt.which : event.keyCode;
+            if (charCode != 46 && charCode > 31
+              && (charCode < 48 || charCode > 57))
+                return false;
+
+            return true;
+        }
+        function noDataKey(evt) {
+            var charcode = (evt.which) ? evt.which : event.keyCode;
+            if (charcode > 0 && charcode < 127)
+                return false;
+
+            return true;
+        }
+
+
+        function noSpaceKey(evt) {
+            limitText(this, 300);
+            if (evt.keyCode != 32) {
+                $("#SummaryError").text("Space is required");
+            }
+            return true;
+
+
+        }
+
+        //$('#Summary').on('keypress', function () {
+        //    limitText(this, 300)
+        //});
+
+        function limitText(field, maxChar) {
+            var ref = $(field),
+                val = ref.val();
+            if (val.length >= maxChar) {
+                ref.val(function () {
+                    console.log(val.substr(0, maxChar))
+                    $("#SummaryError").text("Summary should not be more than 300 words");
+                });
+            }
+        }
+
+
     }
-
-
-function isTextKey(evt) {
-    var charCode = (evt.which) ? evt.which : event.keyCode;
-    if (charCode > 31 && charCode < 65
-      && (charCode < 97 || charCode > 122))
-        return false;
-
-    return true;
-}
-
-function isNumberKey(evt) {
-    var charCode = (evt.which) ? evt.which : event.keyCode;
-    if (charCode != 46 && charCode > 31
-      && (charCode < 48 || charCode > 57))
-        return false;
-
-    return true;
-}
-function noDataKey(evt) {
-    var charcode = (evt.which) ? evt.which : event.keyCode;
-    if (charcode > 0 && charcode < 127)
-        return false;
-
-    return true;
-}
-
-
-function noSpaceKey(evt) {
-    limitText(this, 300);
-    if (evt.keyCode != 32) {
-        $("#SummaryError").text("Space is required");
-    }
-    return true;
-
-
-}
-
-//$('#Summary').on('keypress', function () {
-//    limitText(this, 300)
-//});
-
-function limitText(field, maxChar) {
-    var ref = $(field),
-        val = ref.val();
-    if (val.length >= maxChar) {
-        ref.val(function () {
-            console.log(val.substr(0, maxChar))
-            $("#SummaryError").text("Summary should not be more than 300 words");
-        });
-    }
-}
-
