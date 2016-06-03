967cd36e18b43b39757ed0e100b247f18dcf7575
pacharyazc <pacharya@zerochaos.com>
2016-06-03 14:15:12 +0530

    Upload workitem Javascript Changes

---
+++ DemoWorkBounty/assets/js/uploadworkitem.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    $("#filealertMessage").hide();
+
+});
+
+function upload() {
+    document.getElementById("ButtonSubmit").disabled = false;
+    $("#filesizeerror").text("");
+}
+
+function uploadData() {
+
+    var uploadedFileSize = 0;
+    var uploadedFile = document.getElementById('myFile');
+    if (uploadedFile.value != "") {
+        {
+            uploadedFileSize = uploadedFile.files[0].size;
+            if (uploadedFileSize > 4194304) {
+                $("#filesizeerror").text("File is too big!");
+                document.getElementById("ButtonSubmit").disabled = true;
+                          };
+        }
+
+
+    }
+
+
+
+
+
+}
